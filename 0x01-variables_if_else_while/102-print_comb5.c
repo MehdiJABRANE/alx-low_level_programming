4bf67ed69016df09ebce99b8b12a4b74780670e5
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-19 10:53:08 -0700

    Wed 19 Jul 2023 10:53:08 AM PDT

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j, h, e;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48 + 1; j <= 57; j++)
+		{
+			for (h = i; h <= 57; h++)
+			{
+				for (e = j; e <= 57; e++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(h);
+					putchar(e);
+					if (i + j + e + h != 227)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
