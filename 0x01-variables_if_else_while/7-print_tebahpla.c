826786a2da79fddb3fd23438ebcd35820ea6088c
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-19 09:23:29 -0700

    Wed 19 Jul 2023 09:23:29 AM PDT

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 26; i < 0; i--)
+		putchar(i + 96);
+	putchar('\n');
+	return (0);
+}
