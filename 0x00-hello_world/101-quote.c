921270602551cb13b3e31811ea99bff3420a3451
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-18 17:16:45 -0700

    Tue 18 Jul 2023 05:16:45 PM PDT

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: A C program that prints a sentence without printf nor puts
+ *
+ * Return: Always 1 (Success)
+*/
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
