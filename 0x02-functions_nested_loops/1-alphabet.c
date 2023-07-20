6ad0569c7d606f546f853dc86d2324f7f63c580c
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-20 09:14:59 -0700

    Thu 20 Jul 2023 09:14:59 AM PDT

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with print_alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
