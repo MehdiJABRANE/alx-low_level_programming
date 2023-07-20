883e6f8ff79388bf73ce3a7a0998d53a2bf79389
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-20 10:31:50 -0700

    Thu 20 Jul 2023 10:31:50 AM PDT

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - checks the sign of n
+ *
+ * @n: the number that will be checked
+ *
+ * Return: 1 if it's lower, 0 if it's not
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+}
