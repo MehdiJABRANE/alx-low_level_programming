7c59d3d002a141cc1747e53a1d5b3a6de0557345
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-20 13:27:23 -0700

    Thu 20 Jul 2023 01:27:23 PM PDT

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * times_table - prints the 9 times table
+*/
+
+void times_table(void)
+{
+	int i, j, e, h;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int operation, first_digit, second_digit;
+
+			operation = i * j;
+			if (operation == 0)
+			{
+				_putchar(48);
+			}
+			if (operation > 0 && operation < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(operation + '0');
+			}
+			if (operation > 10)
+			{
+				first_digit = operation / 10;
+				second_digit = operation % 10;
+				_putchar(' ');
+				_putchar(first_digit + '0');
+				_putchar(second_digit + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(44);
+			}
+		}
+		_putchar('\n');
+	}
+}
