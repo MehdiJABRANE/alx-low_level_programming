39bb722cddbe1894639a1c5c726ef0679445ce29
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-20 13:31:38 -0700

    Thu 20 Jul 2023 01:31:38 PM PDT

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(operation + '0');
 			}
-			if (operation > 10)
+			if (operation >= 10)
 			{
 				first_digit = operation / 10;
 				second_digit = operation % 10;
