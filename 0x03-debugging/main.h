c5de339c789ea0aa67804e17eb927e576083aa57
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-22 08:52:43 -0700

    Sat 22 Jul 2023 08:52:43 AM PDT

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,6 @@
 void positive_or_negative(int i);
 long int largest_number(long int a, long int b, long int c);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
