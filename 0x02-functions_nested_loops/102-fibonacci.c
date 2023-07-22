c9e874fea89391079656c5b9cea0a9bd98845430
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-21 18:58:49 -0700

    Fri 21 Jul 2023 06:58:49 PM PDT

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print the sum of all multiplies of 3 and 5
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int a, b, sum;
+
+	a = 0;
+	b = 1;
+	for (i = 0; i < 50; i++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf("%d, ", sum);
+	}
+	return (0);
+}
