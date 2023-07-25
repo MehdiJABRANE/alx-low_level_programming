dbc62f641238cc5f77d3fb3f1e2cceea8c334e9c
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-25 10:30:51 -0700

    Tue 25 Jul 2023 10:30:51 AM PDT

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - A C function that prints a string reversed
+ *
+ * @s: the string
+*/
+
+void print_rev(char *s)
+{
+	int length, i;
+
+	length = _strlen(s);
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
