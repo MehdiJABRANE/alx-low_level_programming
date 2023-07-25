5e00f875fe647b4bb767883343f949b829b2091a
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-25 10:20:08 -0700

    Tue 25 Jul 2023 10:20:08 AM PDT

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - A C function that prints a string
+ *
+ * @str: the string
+*/
+
+void _puts(char *str)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		_putchar(s[length]);
+	}
+}
