562e686c9e75f6a75a61edd8780016407c8f9c1a
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-25 08:55:45 -0700

    Tue 25 Jul 2023 08:55:45 AM PDT

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that changes the value of a pointer to 98
+ *
+ * @n: the pointer
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is README file for the 5th project Pointers, arrays and strings
