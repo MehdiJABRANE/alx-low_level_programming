3abe7d1ec3ab74a501cad6c6047597d125ec26e2
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-26 09:05:32 -0700

    Wed 26 Jul 2023 09:05:32 AM PDT

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - A C function that prints a string
+ *
+ * @str: the string
+*/
+
+void puts_half(char *str)
+{
+	int length, i;
+
+	length = strlen(str);
+	if (length % 2 != 0)
+	{
+		length = (length - 1) / 2;
+		for (i = length; i <= length + length; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		length = length / 2;
+		for (i = length; i < length + length; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
