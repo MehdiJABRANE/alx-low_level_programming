752b8e321a3df56331e1036d902cc272ac8d59ed
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-07-26 08:41:10 -0700

    Wed 26 Jul 2023 08:41:10 AM PDT

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts2 - A C function that prints a string
+ *
+ * @str: the string
+*/
+
+void _puts2(char *str)
+{
+	int length;
+
+	length = 0;
+	while (str[length] != '\0' || str[length - 1] != '\0')
+	{
+		_putchar(str[length]);
+		length += 2;
+	}
+	_putchar('\n');
+}
