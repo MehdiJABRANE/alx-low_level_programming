b763590819185093dce3fbb10dd9feeca24e36cf
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-02 12:02:04 -0700

    Wed 02 Aug 2023 12:02:04 PM PDT

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * factorial - A C function that calculates the factorial
+ *
+ * @n: the number
+ *
+ * Return: the factorial
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
