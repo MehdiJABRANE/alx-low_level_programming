be1bd24b4528ec4b7bf1f77840686be3237265a1
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-02 10:17:43 -0700

    Wed 02 Aug 2023 10:17:43 AM PDT

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _memset - A C function that puts a char in the first n of an array
+ *
+ * @s: the array
+ *
+ * @b: the charater
+ *
+ * @n: the n of array
+ *
+ * Return: The concatenated string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = b[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
