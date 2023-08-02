1152d72728ce9ab06008767dfa8b886865736dda
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-02 10:11:12 -0700

    Wed 02 Aug 2023 10:11:12 AM PDT

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,14 +16,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0;; i++)
-	{
-		if (n == 0)
-			break;
+	for (i = 0; i < n; i++)
+	{		
 		s[i] = b;
-		i--;
 	}
 	return (s);
 }
