534f132103e9cb5658444e3ebe593f3df1bb7b69
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-02 11:44:08 -0700

    Wed 02 Aug 2023 11:44:08 AM PDT

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - A C function that prints a string with recursion
+ *
+ * @s: the string
+*/
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
