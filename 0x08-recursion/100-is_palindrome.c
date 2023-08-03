1919b64af2632e03aa17ceb672fe946a86bbcae2
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-02 18:43:55 -0700

    Wed 02 Aug 2023 06:43:55 PM PDT

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int actual_is_palindrome(char *s, int i, int j);
+
+/**
+ * is_palindrome - returns the 1 if the string is palindrome and 0 if its not
+ * @s: the string
+ *
+ * Return: the result
+ */
+int is_palindrome(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	if (length == 0)
+		return (1);
+	return (actual_is_palindrome(s, 0, length - 1));
+}
+
+/**
+ * actual_is_palindrome - recurses to find if the string is palindrome
+ * @s: the string
+ * @i: i
+ * @j: j
+ *
+ * Return: the result
+ */
+int actual_is_palindrome(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		if (j == 0)
+			return (1);
+		return (actual_is_palindrome(s, i + 1, j - 1));
+	else
+		return (0);
+}
