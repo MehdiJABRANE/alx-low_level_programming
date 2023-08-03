0dc9818fa29c4713129a61a8944cdfec12d96d0a
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-02 18:55:29 -0700

    Wed 02 Aug 2023 06:55:29 PM PDT

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+int actual_wildcmp(char *s1, char *s2, int i);
+
+/**
+ * wildcmp - returns the 1 if the strings is identical and 0 if its not
+ * @s1: the first string
+ * @s2: the second one
+ *
+ * Return: the result
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (actual_wildcmp(s1, s2, 0));
+}
+
+/**
+ * actual_wildcmp - recurses to find if the 2 strings identical
+ * @s1: the first string
+ * @s2: the second one
+ * @i: i
+ *
+ * Return: the result
+ */
+int actual_wildcmp(char *s1, char *s2, int i)
+{
+	if (s2[i] == '*')
+	{
+		if (j == strlen(s2))
+			return (1);
+		return (actual_wildcmp(s1, s2, i + 1));
+	}
+	if (s1[i] == s2[i])
+	{
+		if (j == strlen(s2))
+			return (1);
+		return (actual_wildcmp(s1, s2, i + 1));
+	}
+	else
+		return (0);
+}
