839910b812e979e3166a44838e654ef99e859f69
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-04 10:16:28 -0700

    Fri 04 Aug 2023 10:16:28 AM PDT

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: int
+ * @argv: list
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv[];
+	printf("%d\n", argc0);
+	return (0);
+}
