69f36af79645207d058e78863757e8a2b20e7463
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-07 10:01:40 -0700

    Mon 07 Aug 2023 10:01:40 AM PDT

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - A C function that creates an array
+ * @str: the array of characters
+ * Return: the array (success)
+*/
+char *_strdup(char *str)
+{
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *cpstr = (char *)malloc(strlen(str) * sizeof(char));
+
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < strlen(str); i++)
+			{
+				cpstr[i] = str[i];
+			}
+			return (cpstr);
+		}
+	}
+}
