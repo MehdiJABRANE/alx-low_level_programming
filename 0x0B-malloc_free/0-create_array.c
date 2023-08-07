8d29204d6959fe1c4732799305df82d8aa5bf6db
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-07 09:23:29 -0700

    Mon 07 Aug 2023 09:23:29 AM PDT

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,8 @@
 */
 char *create_array(unsigned int size, char c)
 {
+	int i;
+
 	if (size == 0)
 	{
 		return (NULL);
@@ -24,7 +26,10 @@ char *create_array(unsigned int size, char c)
 		}
 		else
 		{
-			arr[0] = c;
+			for (i = 0; i < size; i++)
+			{
+				array_calloc[i] = c;
+			}
 			return (arr);
 		}
 	}
