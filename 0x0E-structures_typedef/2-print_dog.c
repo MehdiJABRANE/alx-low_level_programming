1cb58776e7061f397dfc40f642551d1376ee6e43
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-15 17:25:18 -0700

    Tue 15 Aug 2023 05:25:18 PM PDT

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - A C function that add variables to dog struct
+ * @d: the struct
+*/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
