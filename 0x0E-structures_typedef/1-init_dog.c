ccbda69cf3de65a7c2a8beefe457bb037957bbe7
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-15 17:18:27 -0700

    Tue 15 Aug 2023 05:18:27 PM PDT

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - A C function that add variables to dog struct
+ * @d: the struct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
