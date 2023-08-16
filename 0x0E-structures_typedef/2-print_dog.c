8c9199bf1ab8a436850204a9e69d82cfc8d48a9b
Mehdi JABRANE <jabranemehdi088@gmail.com>
2023-08-15 17:35:54 -0700

    Tue 15 Aug 2023 05:35:54 PM PDT

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,9 @@
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
-	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: \(nil\)");
-		printf("Age: %f\n", d->age);
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: \(nil\)");
+    	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
